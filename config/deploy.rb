be36e1f8fdc71825ec02edf94339dc2af694fc15
vibhoo <vibhoo@ymail.com>
2012-10-11 22:37:10 +0530

    FourthChanges

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,12 +8,6 @@ set :application, "Vbihoo"
 
 set :scm, :git
 set :repository, "git@github.com:vibhoo/Codes.git"
-set :scm_passphrase,""
+set :scm_passphrase,"vibhooini9"
 
 set :user,"root"
-
-role :web, "your web-server here"                          # Your HTTP server, Apache/etc
-role :app, "your app-server here"                          # This may be the same as your `Web` server
-role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
-role :db,  "your slave db-server here"
-
