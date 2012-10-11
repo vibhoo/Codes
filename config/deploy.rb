7b2197a5c7769b8cfd1df5d283b1dcf878021936
Vibhoo <mishrarock2010@gmail.com>
2012-10-11 23:06:47 +0530

    Update config/deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,13 +1,10 @@
-require 'capistrano/ext/multistage'
-
-set :stages, ["staging","production"]
-set :default_stage, "staging"
 
 set :application, "Vbihoo"
-
-
 set :scm, :git
 set :repository, "git@github.com:vibhoo/Codes.git"
 set :scm_passphrase,""
-
 set :user,"root"
+
+require 'capistrano/ext/multistage'
+set :stages, ["staging","production"]
+set :default_stage, "staging"
